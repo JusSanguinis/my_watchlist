1855ce11531f4e093d4cd7ba47297b8b605712ee
memmeat <myildirim33@outlook.com.tr>
2022-08-23 23:09:37 +0300

    Api'ye ulaşılıp veriler alınabilir hale geldi.

--- src/components/Add.js
+++ src/components/Add.js
@@ -2,7 +2,17 @@ import React, { useState } from 'react';
 
 const Add = () => {
   const [query, setQuery] = useState('');
-  console.log(query);
+  const [results, setResults] = useState([]);
+
+  const onChange = (e) => {
+    setQuery(e.target.value);
+    fetch(
+      `https://api.themoviedb.org/3/search/movie?api_key=9e7bb79c1e7b7ad458568637c0dbdfe4&language=en-US&page=1&include_adult=false&query=${e.target.value}`
+    )
+      .then((res) => res.json())
+      .then((data) => setResults(data.results));
+  };
+
   return (
     <div className="add-page">
       <div className="container">
@@ -16,15 +26,18 @@ const Add = () => {
             <h2>
               Milyonlarca film, TV şovu ve keşfedilecek kişi. Şimdi keşfedin.
             </h2>
-            {query}
           </div>
           <div className="input-wrapper">
             <input
               type="text"
               placeholder="Film, dizi, kişi ara..."
-              onChange={(e) => setQuery(e.target.value)}
+              value={query}
+              onChange={onChange}
             />
           </div>
+          {results.map((movie) => (
+            <h1>{movie.title}</h1>
+          ))}
         </div>
       </div>
     </div>
