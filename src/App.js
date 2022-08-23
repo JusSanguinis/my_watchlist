4337473629ab961233fd727e9953c1117de1aebe
memmeat <myildirim33@outlook.com.tr>
2022-08-23 21:52:34 +0300

    Route işlemleri yapıldı.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import './App.css';
+import Header from './components/Header';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import WatchList from './components/WatchList';
+import Watched from './components/Watched';
 
 function App() {
-  return <div>Deneme</div>;
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<WatchList />} />
+        <Route path="/watched" element={<Watched />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
